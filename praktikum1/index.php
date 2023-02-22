be4646eb15ebb9f00a61608957df5cdb5e548f57
amirsiregar05 <amirnecis17@gmail.com>
2023-02-22 15:39:13 +0700

    upload praktikum1

---
+++ praktikum1/index.php
@@ -0,0 +1,43 @@
+<?php 
+// ini sebuah komentar
+
+
+
+
+
+
+
+// echo "Hello Word <br>";
+// echo 'Hello Word <br>';
+// print_r("Adam Yusron <br>");
+// var_dump("STT Nurul Fikri");
+
+// membuat variabel user
+$nama = "Adam Malik";
+$umur = 20;
+$berat = 70.5;
+$mahasiswa = true;
+
+// echo "Hallo, Nama saya $nama ";
+// echo "Umurku $umur th ";
+// echo "dan memiliki berat badan $berat kg";
+
+// membuat variabel sistem
+// echo "Document root " . $_SERVER['DOCUMENT_ROOT'];
+// echo "<br>";
+// echo "Nama file " . $_SERVER['PHP_SELF'];
+
+// membuat variabel konstanta
+// define('PHI', 3.14);
+// $jari = 8;
+// $luas = PHI * $jari * $jari;
+// $keliling = 2 * PHI * $jari;
+// echo "<br> Luas lingkaran degan jari-jari $jari= $luas";
+// echo "<br> Luas lingkaran degan jari-jari $jari= $keliling";
+
+
+// membuat array
+$programs = ["Php", "Javascript", "HTML", "CSS"];
+// echo $programs[3];
+echo "jumlah data sebanyak " . count($programs);
+?>
