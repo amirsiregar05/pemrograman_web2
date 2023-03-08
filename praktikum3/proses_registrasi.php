45644fac84d2dc7715868584c049baa28773c6a6
amirsiregar05 <amirnecis17@gmail.com>
2023-03-08 16:00:23 +0700

    upload praktik 3

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php 
+
+$domisili = [" Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informasi",
+    "BD"=>"Bisnis Digital"
+];
+
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+];
+
+?>
